147e62883bb237be9cba8a34cf7b5877b14b4366
Evelin-montoya <evelinlamejor2004@gmail.com>
2023-12-04 15:35:09 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entradas del problema
+        String nombreUsuario="Carlos Andres Urango Oviedo";
+        short edadUsuario=34;
+        String telefonoUsuario="3015137234";
+        String documentoIdentidad="1193096819";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario="Cll 50# 80-20";
+        double costoInscripcion=150000.0;
+        float tasaInteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+        //PROCESO
+
+        //1. Asignar un valor especifico a una variable
+        //desde el TECLADO (Recibir datos)
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre?");
+        //leerDato.nextLine();
+
+
+    }
+}
